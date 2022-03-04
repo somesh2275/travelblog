b1312ebd9c7bbf142f3e0bc14946f650bef36a1e
Somesh8600 <somesh@oneagency.co>
2022-03-04 17:13:07 +0000

    initial commit

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use App\Http\Requests\UserRequest;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the users
+     *
+     * @param  \App\Models\User  $model
+     * @return \Illuminate\View\View
+     */
+    public function index(User $model)
+    {
+        return view('users.index', ['users' => $model->paginate(15)]);
+    }
+
+    public function show(User $model)
+    {
+        return view('users.show', ['users' => $model->paginate(15)]);
+    }
+}
---
+++ resources/views/layouts/footers/guest.blade.php
@@ -0,0 +1,11 @@
+<footer class="footer">
+  <div class="container-fluid">
+    <div class="copyright float-right">
+      &copy;
+      <script>
+        document.write(new Date().getFullYear())
+      </script>, made with <i class="material-icons">favorite</i> by
+      <a href="https://www.webtechsavvy.co.in" target="_blank">Web Tech Savvy</a> for a better web.
+    </div>
+  </div>
+</footer>
